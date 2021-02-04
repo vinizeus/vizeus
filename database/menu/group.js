5478227bf71efb7f0eb8dba84208d911c4ef43fe
vinizeus <viniciusonezio35@gmail.com>
2021-02-04 01:13:40 -0400

    First commit

---
+++ database/menu/group.js
@@ -0,0 +1,45 @@
+const group = (pushname, prefix, botName, ownerName) => {
+        return `
+╔══✪〘 Informações 〙✪══
+║
+║───────⊹⊱✫⊰⊹───────
+║➩ ❍ wa.me/556993899391
+║➩ ❍ Prefix: 「  ${prefix}  」
+║➩ ❍ Criador: ${botName}
+║➩ ❍ Nome: ${pushname}️
+║➩ ❍ XP: ${reqXp}
+║➩ ❍ Money: ${uangku}
+───────⊹⊱✫⊰⊹───────
+
+
+                     𝚉𝙴𝚄𝚂
+
+───────⊹⊱✫⊰⊹───────
+║➩ ❍ *${prefix}info*
+║➩ ❍ *${prefix}blocklist*
+║➩ ❍ *${prefix}chatlist*
+║➩ ❍ *${prefix}ping*
+║➩ ❍ *${prefix}bugreport*
+║➩ ❍ *${prefix}opengc*
+║➩ ❍ *${prefix}closegc*
+║➩ ❍ *${prefix}promote*
+║➩ ❍ *${prefix}demote*
+║➩ ❍ *${prefix}tagall*
+║➩ ❍ *${prefix}tagall2*
+║➩ ❍ *${prefix}tagall3*
+║➩ ❍ *${prefix}tagall4*
+║➩ ❍ *${prefix}tagall5*
+║➩ ❍ *${prefix}add*
+║➩ ❍ *${prefix}kick*
+║➩ ❍ *${prefix}listadmins*
+║➩ ❍ *${prefix}linkgroup*
+║➩ ❍ *${prefix}leave*
+║➩ ❍ *${prefix}welcome*
+║➩ ❍ *${prefix}nsfw*
+║➩ ❍ *${prefix}delete*
+║➩ ❍ *${prefix}simih*
+║➩ ❍ *${prefix}ownergroup*
+║➩ Aumente seu level interagindo no grupo!!
+───────⊹⊱✫⊰⊹───────`
+}
+exports.group = group
